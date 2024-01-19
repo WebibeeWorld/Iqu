75c82d30bebb43e60d25cd9574bbd51381179f66
unknown <gokulgandhi97@gmail.com>
2024-01-19 20:39:38 +0530

    wip

--- components/CounsellingServices.tsx
+++ components/CounsellingServices.tsx
@@ -10,7 +10,7 @@ const CounsellingServices= () => {
     <Element className="h-full" name="testimonial" >
       <div className="relative h-full">
       <div className="relative h-[400px] md:h-[350px]">
-        <Image src={"/img-1.png"} alt="image" fill className="object-cover object-center" />
+        <Image src={"/therapy2.jpg"} alt="image" fill className="object-cover object-center" />
       </div>
       <div className="absolute top-0 left-0 w-full h-full flex flex-col space-y-4 px-4">
       <h3 className="capitalize text-3xl  w-full mx-auto tracking-wide font-Montserrat font-bold text-white text-center p-4">Counseling services</h3>
