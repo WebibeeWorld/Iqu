c81ae4092b2133a4c8215559e0db544595d67e38
Prithvi <prithvin19@gmail.com>
2024-01-25 22:40:36 +0530

    Layout grp for studio

---
+++ app/(users)/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata } from "next";
+import { Providers } from "../provider";
+import Footer from "@/components/Footer";
+import FloatingButton from "@/components/FloatingButton";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Lato, Montserrat } from "next/font/google";
+const ExitModalComponent = dynamic(
+  () => import("@/components/ExitModal") as any,
+  { ssr: false }
+);
+import Contact from "@/components/Contact";
+import dynamic from "next/dynamic";
+import DefaultNavbvar from "@/components/DefaultNavbvar";
+import { FAQServer } from "@/components/layout/FAQServer";
+
+export const metadata: Metadata = {
+  title: "IQU Clinic",
+  description: "Therapy & Counseling Center",
+};
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-montserrat",
+});
+
+const lato = Lato({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-lato",
+  weight: "400",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={`${lato.variable} ${montserrat.variable}`}>
+      <body className="font-lora">
+        <Providers>
+          <ExitModalComponent />
+          <DefaultNavbvar />
+          <main className="relative overflow-hidden">{children}</main>
+          <FloatingButton />
+          <FAQServer />
+          <Contact />
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  );
+}
