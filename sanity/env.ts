652e4a8662a744d395a9e41d2e82e8a0fab66493
Prithvi <prithvin19@gmail.com>
2024-10-09 14:23:17 +0530

    t

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,7 +11,7 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
-export const useCdn = false
+export const useCdn = true; // Enable CDN usage
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
